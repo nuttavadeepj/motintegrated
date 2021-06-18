21a754cd4b00c7fb994c54a64b49c6be2812bda3
nuttavadeepj <nuttavadee.pj@mail.kmutt.ac.th>
2021-06-18 18:32:41 +0700

    [CHANGE] About detail

--- lib/screens/aboutus.dart
+++ lib/screens/aboutus.dart
@@ -57,7 +57,7 @@ class AboutUs extends StatelessWidget {
                                   children: [
                                     TextSpan(
                                         text:
-                                            ' is a service that encourages you to separate waste. You just use our trash bags and we will receive a compostable waste from you. You send us the waste then we will have many special deals for you :)',
+                                            ' is a service that encourages you to separate waste. Application for receiving and managing biodegradable waste (food waste) from households, markets, and small restaurants. You just use our trash bags and we will receive a compostable waste from you. You send us the waste then we will have many special deals for you :)',
                                         style: TextStyle(
                                           fontFamily: 'Jost',
                                           height: 1.3,
